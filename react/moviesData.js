462053264442eb458ce06d5305c6c73426368d46
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-02 16:47:11 +0200

    Create moviesData.js

---
+++ react/moviesData.js
@@ -0,0 +1,9 @@
+// moviesData.js
+const moviesData = [
+  { id: 1, title: 'Movie 1', poster_path: 'path/to/poster1.jpg', rating: 5 },
+  { id: 2, title: 'Movie 2', poster_path: 'path/to/poster2.jpg', rating: 3 },
+  { id: 3, title: 'Movie 3', poster_path: 'path/to/poster3.jpg', rating: 2 },
+  // Add more movies with different ratings
+];
+
+export default moviesData;
