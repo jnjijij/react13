415aad78bea7070794e11af5553ad1e81a5586ed
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-02 17:23:41 +0200

    Create 87878MoviesListCard.js

---
+++ react/87878MoviesListCard.js
@@ -0,0 +1,22 @@
+// MoviesListCard.js
+import React from 'react';
+import { Card, CardImg, CardBody, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import StarsRating from './StarsRating';
+
+const MoviesListCard = ({ movie }) => {
+  const { title, poster_path } = movie;
+
+  return (
+    <Card className="movie-card">
+      <Link to={`/movies/${movie.id}`}>
+        <CardImg top src={`/images/${poster_path}`} alt={title} />
+        <CardBody>
+          <CardTitle>{title}</CardTitle>
+        </CardBody>
+      </Link>
+    </Card>
+  );
+};
+
+export default MoviesListCard;
