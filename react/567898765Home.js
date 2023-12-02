514cba7e68bffde03f8955193db14701c5b27c7c
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-02 13:30:58 +0200

    Create 567898765Home.js

---
+++ react/567898765Home.js
@@ -0,0 +1,38 @@
+// Home.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Home.css'; // Import your custom styles
+
+const Home = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://cssauthor.com/pinball-responsive-grid-style-blog-psd/');
+        const parser = new DOMParser();
+        const htmlDocument = parser.parseFromString(response.data, 'text/html');
+        const imageElements = htmlDocument.querySelectorAll('.blog-post img');
+        const imageSrcArray = Array.from(imageElements).map((img) => img.src);
+        setImages(imageSrcArray);
+      } catch (error) {
+        console.error('Error fetching images:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="home-container">
+      <h2>Featured Images</h2>
+      <div className="image-grid">
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`Image ${index}`} className="grid-item" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
